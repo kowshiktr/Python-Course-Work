73d9e30567f418c6b6a3eb6c8e852bddc4f45d96
kowshiktr <talapulakowshik@gmail.com>
2025-07-25 21:43:31 +0530

    add

---
+++ Classwork/operators.py
@@ -0,0 +1,14 @@
+# 2 Variable Rules and Assignment
+myvar=10 # valid var
+myvar # 10
+Myvar=20 # valid var
+Myvar # 20
+ # 1var=30 # SyntaxError: 
+ # @myvar=40 # SyntaxError: 
+my_var=50 # valid var
+my_var # 50
+ # my@var=60 # SyntaxError: 
+
+str1=15 # case-sensistive
+Str1=20 #case-sensistive
+print(str1,Str1) # 15 20
